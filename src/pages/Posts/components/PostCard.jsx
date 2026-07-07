2f7d880444fe1db5880ad0d18c4cb00e0ba357d9
raoofe <raoofp337@gmail.com>
2026-07-07 10:19:23 +0530

    dd

--- src/pages/Posts/components/PostCard.jsx
+++ src/pages/Posts/components/PostCard.jsx
@@ -5,10 +5,10 @@ export const PostCard = ({
     post
 }) => {
   return (
-   <div className="flex justify-center px-4 py-6">
+   <div className="flex justify-center px-4 py-6 ">
   <a
     href="#"
-    className="w-full max-w-lg p-4 sm:p-6 border border-default rounded-base shadow-xs hover:bg-neutral-secondary-medium bg-neutral-primary-soft"
+    className="w-full max-w-lg p-4 sm:p-6   rounded-3xl  hover:bg-neutral-secondary-medium bg-neutral-primary-soft bg-blue-400"
   >
     <h5 className="mb-3 text-xl sm:text-2xl font-semibold tracking-tight leading-7 sm:leading-8 text-heading">
       {post.title}
